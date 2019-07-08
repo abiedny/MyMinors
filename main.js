4f9fa30c0f9332c27aa770a24de86a26983a63f2
Alex Biedny <biedn001@umn.edu>
2019-07-08 12:45:30 -0500

    adding files

---
+++ main.js
@@ -0,0 +1,8 @@
+let possibleTables = document.getElementsByClassName("subreqTitle");
+
+creditHeaders = Array.from(possibleTables).filter(table => {
+    const reg = /\d{1,3} total credits including transfer work/;
+    return reg.match(table.innerHTML);
+});
+
+let classes = Array.from(creditHeaders[0].nextElementSibling.nextElementSibling.children[1].children);
